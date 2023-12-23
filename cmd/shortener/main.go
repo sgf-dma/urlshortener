f2adeee9723dc3f7d1f1a4b8cc84eb1bad8be0f5
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2023-12-23 20:28:21 +0300

    iter1: Set up a basic server on port 8080

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("blah"))
+	})
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
